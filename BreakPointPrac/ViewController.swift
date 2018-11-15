39b77d7ed86e71fa260220c51a5d49f957feb98a
Jayyoung Yang <jay_y1711@hotmail.com>
2018-11-15 12:08:33 +0900

    Initial Commit

---
+++ BreakPointPrac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BreakPointPrac
+//
+//  Created by Jayyoung Yang on 15/11/2018.
+//  Copyright © 2018 Jayyoung Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var sum = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var count = 0
+        for row in 5...10 {
+            count += 1
+            self.sum += row
+        }
+        print("총 합은 \(self.sum), \(count)회 실행되었습니다.")
+    }
+}
+
